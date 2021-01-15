c05a57c76e677068ce1377a97aa2d671c04a2e59
minsoo-web <zlemzlem5656@naver.com>
2021-01-15 18:16:23 +0900

    [추가] npm run eject 로 인한 디폴트 theme 가져오기

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,8 @@ module.exports = {
     logo:
       "https://avatars1.githubusercontent.com/u/57122180?s=460&u=b32867107508426379c28b1d0c2fd8963912a5dd&v=4",
     nav: [{ text: "Home", link: "/" }],
-    sidebar: "auto"
+    sidebar: "auto",
+    smoothScroll: true // 부드러운 스크롤 사용 여부
   },
 
   configureWebpack: {
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,11 @@
 # hello
 
+```JavaScript
+const a = 10;
+function foo() {
+    console.log('블로그 만드는 중')
+}
+
+```
+
 ![image](~@/default.jpeg)
