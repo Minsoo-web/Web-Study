80ab74ac3056f9e10086664876b9f0d0faa848f4
minsoo-web <zlemzlem5656@naver.com>
2021-01-20 23:46:53 +0900

    [추가] 설정파일 변경

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  title: "Hello VuePress",
+  title: "minsoo-web.log",
   description: "Just playing around",
   themeConfig: {
     logo:
       "https://avatars1.githubusercontent.com/u/57122180?s=460&u=b32867107508426379c28b1d0c2fd8963912a5dd&v=4",
-    nav: [{ text: "Home", link: "/" }],
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Github", link: "https://github.com/Minsoo-web" }
+    ],
     sidebar: "auto",
     smoothScroll: true // 부드러운 스크롤 사용 여부
   },
